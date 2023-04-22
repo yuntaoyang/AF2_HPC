0dd6f3edc87835e395b5869db506d87a279c438c
yuntaoyang <uestcyangyuntao@gmail.com>
2023-04-22 11:10:53 -0400

    update script

---
+++ structure.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import shutil
+
+def main():
+    # directory for pdb files
+    if not os.path.exists('./pdb/'):
+        os.mkdir('./pdb/')   
+    # generate pdb files
+    f = open("error_parafold", 'a')
+    for folder in os.listdir('./out/'):
+        files = os.listdir(os.path.join('./out/',folder))
+        if 'ranked_0.pdb' in files:
+            shutil.copy2(os.path.join('./out/',folder,'ranked_0.pdb'),'./pdb/')
+            os.rename(os.path.join('./pdb/','ranked_0.pdb'),os.path.join('./pdb/',folder+'.pdb'))
+        else:
+            f.write('Error: '+folder)
+    f.close()
+    
+if __name__ == "__main__":
+    main() 
