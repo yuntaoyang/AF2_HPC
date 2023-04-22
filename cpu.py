6852117721511cb9247ab8e1062b280b5eb64e90
yuntaoyang <uestcyangyuntao@gmail.com>
2023-04-22 11:04:13 -0400

    update script

--- cpu.py
+++ cpu.py
@@ -1,36 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-af2_script = '/work/07225/yang1995/ls6/software/ParallelFold_212/run_alphafold.sh'
-af2_data = '/scratch/tacc/apps/bio/alphafold/data'
-path_out = ''
-path_fasta = ''
-path_log = ''
-
-max_template_date = '2022-03-10'
-file_name = '' # a csv file with a column for file name and a column for length
-
-# 2 parameters for monomer and multimer
-# 'monomer_ptm' & 'model_1'
-# 'multimer' & 'model_1_multimer'
-parameter_1 = ''
-parameter_2 = ''
+# Generate a job file for TACC computing clusters
+# --s: the directory of AF2 script
+# --d: the directory of AF2 data
+# --m: monomer or multimer
+# --t: max template date
 
 import os
 import pandas as pd
+import argparse
 
-if __name__ == "__main__":
-    job = pd.read_csv(file_name)
+def script(file,arg_s,arg_d,p,arg_t,m):
+    script = ['bash',os.path.join(arg_s,'run_alphafold.sh'),
+              '-d',arg_d,
+              '-o','./out/',
+              '-p',p,
+              '-i',os.path.join('./fasta/',file),
+              '-t',arg_t,
+              '-m',m,
+              '-f','>',
+              os.path.join('./log_cpu/',file.replace('.fasta','.log')),'2>&1']
+    return ' '.join(script)
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--s', required=True)
+    parser.add_argument('--d', required=True)
+    parser.add_argument('--m', required=True)
+    parser.add_argument('--t', required=True)
+    return parser
+
+def main():
+    # Set up parameters
+    parser = get_parser()
+    args = parser.parse_args()
+    arg_s = args.s
+    arg_d = args.d
+    arg_m = args.m
+    arg_t = args.t
+    if arg_m == 'monomer':
+        m = 'model_1'
+        p = 'monomer_ptm'
+    if arg_m == 'multimer':
+        m = 'model_1_multimer'
+        p = 'multimer'
+    # Create directory for output and log
+    if not os.path.exists('./out/'):
+        os.makedirs('./out/')
+    if not os.path.exists('./log_cpu/'):
+        os.makedirs('./log_cpu/')
+    # Load the job list
+    df = pd.read_csv('jobs.csv')
+    df = df.sort_values(by=['length'])
+    # Generate a job file for TACC
     f = open("jobfile", 'a')
-    for file in job['file']:
-        line = ['bash',af2_script,
-                '-d',af2_data,
-                '-o',path_out,
-                '-p',parameter_1,
-                '-i',path_fasta+file+'.fasta',
-                '-t',max_template_date,
-                '-m',parameter_2,
-                '-f','>',
-                path_log+file+'.log','2>&1']
-        f.write(' '.join(line)+'\n')
+    for file in df['file']:
+        f.write(script(file,arg_s,arg_d,p,arg_t,m)+'\n')
     f.close()
+            
+if __name__ == "__main__":
+    main()    
